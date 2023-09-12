00eb04c21e2855d17b8beb0d4502be5cd5a923d7
lswdev <lswdev@gmail.com>
2023-09-13 00:29:42 +0900

    1. vue cli 적용 2. vue 2.0 version 3. TodoHeader.vue / TodoInput.vue생성

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <TodoHeader />
+    <TodoInput />
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoHeader, TodoInput
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    inputs
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TodoInput',
+  }
+</script>
+
+<style>
+  
+</style>
