0a5b5b3f2cab6e8a25d25afae5b3d1535fee7909
lswsepia <lswsepia@gmail.com>
2023-10-16 16:01:17 +0900

    const , let 변수 적용

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -12,7 +12,6 @@ import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoFooter from './components/TodoFooter.vue'
 import TodoList from './components/TodoList.vue'
-
 export default {
   name: 'App',
   components: { TodoHeader, TodoInput, TodoFooter, TodoList },
@@ -21,7 +20,7 @@ export default {
   }),
   methods: {
     addOneItem (todoItem) {
-      let obj = {completed: false, item: todoItem};
+      const obj = {completed: false, item: todoItem};
       // 로컬스토리지에 저장  lacalStorage.setItem( key, value );
         localStorage.setItem(todoItem, JSON.stringify(obj));  //JSON.stringify 는 객체인 obj를 string으로 변환시켜주는 API.
         this.todoItems.unshift(obj); //저장하는 로직  // 참고사이트 : https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
@@ -42,7 +41,7 @@ export default {
   },
   created() {
     if(localStorage.length > 0) {
-      for(var i = 0; i < localStorage.length; i++) {
+      for(let i = 0; i < localStorage.length; i++) {
         if(localStorage.key(i) !== '' && localStorage.key(i) !== 'loglevel:webpack-dev-server'){
           this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));  // JSON.stringify로 인해 string 처리된 데이터를 parsing 해준다.
         }
