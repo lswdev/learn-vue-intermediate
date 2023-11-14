dfef12921fec81aec66b0f26744fe955811e2710
lswsepia <lswsepia@gmail.com>
2023-11-14 16:08:08 +0900

    .

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -29,12 +29,12 @@ export const store = new Vuex.Store({
   },
   mutations: {
     addOneItem (state, todoItem) {
-      const obj = {completed: false, item: todoItem};
+      const obj = { completed: false, item: todoItem };
       // 로컬스토리지에 저장  lacalStorage.setItem( key, value );
       localStorage.setItem(todoItem, JSON.stringify(obj));  //JSON.stringify 는 객체인 obj를 string으로 변환시켜주는 API.
       state.todoItems.unshift(obj); //저장하는 로직  // 참고사이트 : https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
     },
-    removeOneItem (state, {todoItem, index}) {
+    removeOneItem (state, { todoItem, index }) {
       localStorage.removeItem(todoItem.item);  // Object 내부 문자열 선택
       state.todoItems.splice(index, 1);  //특정 index에서 하나를 지운다  // slice는 기존 배열을 변경한다.  // splice는 기존 배열을 유지한다.
     },
